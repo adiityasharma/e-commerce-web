55088df91935b31318511ec0dc41e8491e9f9700
Aditya Sharma <adityasharma626367@gmail.com>
2025-07-30 00:05:09 +0530

    added header , sidebar and made ui responsive

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -1,34 +1,85 @@
-import { CircleUser, Settings } from 'lucide-react';
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import {
+  BaggageClaim,
+  LayoutDashboard,
+  Settings,
+  ShoppingBasket,
+} from "lucide-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard size={22} />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket size={22} />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BaggageClaim size={22} />,
+  },
+];
 
-const MeneItems = () => {
-
-  return (
-    <nav className='mt-8 flex-col flex gap-2 '>
-
-    </nav>
-  )
-}
-
-
-
-const AdminSidebar = () => {
-
+const MeneItems = ({ setOpen }) => {
   const navigate = useNavigate();
 
+  return (
+    <nav className="mt-8 flex-col flex gap-2 ">
+      {adminSidebarMenuItems.map((menuItems) => (
+        <div
+          onClick={() => {
+            navigate(menuItems.path);
+            setOpen ? setOpen(false) : null;
+          }}
+          key={menuItems.id}
+          className="flex items-center gap-2 rounded-md px-3 py-2 cursor-pointer text-sm font-semibold hover:bg-gray-100"
+        >
+          {menuItems?.icon}
+          <span>{menuItems.label}</span>
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+const AdminSidebar = ({ open, setOpen }) => {
+  const navigate = useNavigate();
 
   return (
     <>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <SheetHeader className="border-b">
+            <SheetTitle className="flex items-center gap-2 text-lg font-semibold">
+              <Settings />
+              Admin Panel
+            </SheetTitle>
+          </SheetHeader>
+          <MeneItems setOpen={setOpen} />
+        </SheetContent>
+      </Sheet>
+
       <aside className="hidden flex-col  w-64 border-r bg-background p-6 lg:flex  ">
-        <div onClick={()=>navigate("/admin/dashboard")} className="flex items-center gap-2 cursor-pointer">
+        <div
+          onClick={() => navigate("/admin/dashboard")}
+          className="flex items-center gap-2 cursor-pointer"
+        >
           <Settings />
-          <h1 className='text-lg font-semibold '>Admin Panel</h1>
+          <h1 className="text-lg font-semibold ">Admin Panel</h1>
         </div>
+        <MeneItems />
       </aside>
     </>
   );
-}
+};
 
-export default AdminSidebar
+export default AdminSidebar;
--- client/src/config/formControl.js
+++ client/src/config/formControl.js
@@ -1,3 +1,4 @@
+
 export const RegisterFormControl = [
   {
     name: "username",
@@ -38,20 +39,3 @@ export const LoginFormControl = [
   },
 ]
 
-export const adminSidebarMenuItems = [
-  {
-    id: "dashboard",
-    label: "Dashboard",
-    path: "/admin/dashboard"
-  },
-  {
-    id: "products",
-    label: "Products",
-    path: "/admin/products"
-  },
-  {
-    id: "orders",
-    label: "Orders",
-    path: "/admin/orders"
-  },
-]
