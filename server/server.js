de7f454574798427c5571e5a79942dc7d55d1bd7
Aditya Sharma <adityasharma626367@gmail.com>
2025-08-13 01:47:58 +0530

    .

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: "*",
+  origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [
     "Content-Type",
@@ -46,7 +46,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/admin/products", adminProductRouter)
