7c5e02880574afc479940e1c7f7392fe2698129b
Aditya Sharma <adityasharma626367@gmail.com>
2025-08-13 01:28:36 +0530

    add cors new origin

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const app = express();
 const PORT = process.env.PORT || 3002
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://ecommerce-three-black-47.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [
     "Content-Type",
